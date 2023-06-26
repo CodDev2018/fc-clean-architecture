1b91f094846b828be99fe32d5d22656f498c9e95
Paulo Bezerra <bezerra.paulo@fcamara.com.br>
2023-06-26 09:15:26 -0400

    adicionando tipagem aos handlers

--- src/domain/customer/event/handler/log-when-custumer-address-is-updated.handler.ts
+++ src/domain/customer/event/handler/log-when-custumer-address-is-updated.handler.ts
@@ -1,7 +1,8 @@
 import EventHandlerInterface from "../../../@shared/events/event-handler.interface";
+import CustomerAddressUpdatedEvent from "../customer-address-updated.event";
 
 export default class LogWhenCustomerAddressUpdatedHandler implements EventHandlerInterface {
-  handle(event: any): void {
+  handle(event: CustomerAddressUpdatedEvent): void {
     const {id, name, address } = event.eventData;
     const {street, number, city} = address;
     console.log(`Endereço do cliente: ${id}, ${name} alterado para: ${street}, ${number}, ${city}.`);
--- src/domain/customer/event/handler/log2-when-custumer-is-created.handler.ts
+++ src/domain/customer/event/handler/log2-when-custumer-is-created.handler.ts
@@ -1,7 +1,8 @@
 import EventHandlerInterface from "../../../@shared/events/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
 
 export default class Log2WhenCustomerIsCreatedHandler implements EventHandlerInterface {
-  handle(event: any): void {
+  handle(event: CustomerCreatedEvent): void {
     console.log('Esse é o segundo console.log do evento: CustomerCreated');
   }
 }
